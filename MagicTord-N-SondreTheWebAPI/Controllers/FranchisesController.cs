f8fe7ca2b8eba5d7d3efd8a80d52694ea77f9f0d
SondreSequence <sondre.kristoffer.reinholdtsen@no.experis.com>
2023-02-28 08:48:25 +0100

    feat. controllers

---
+++ MagicTord-N-SondreTheWebAPI/Controllers/FranchisesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MagicTord_N_SondreTheWebAPI.Models;
+
+namespace MagicTord_N_SondreTheWebAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class FranchisesController : ControllerBase
+    {
+        private readonly DBContext _context;
+
+        public FranchisesController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Franchises
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Franchise>>> GetFranchise()
+        {
+            return await _context.Franchise.ToListAsync();
+        }
+
+        // GET: api/Franchises/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Franchise>> GetFranchise(int id)
+        {
+            var franchise = await _context.Franchise.FindAsync(id);
+
+            if (franchise == null)
+            {
+                return NotFound();
+            }
+
+            return franchise;
+        }
+
+        // PUT: api/Franchises/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFranchise(int id, Franchise franchise)
+        {
+            if (id != franchise.FranchiseID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(franchise).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FranchiseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Franchises
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Franchise>> PostFranchise(Franchise franchise)
+        {
+            _context.Franchise.Add(franchise);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFranchise", new { id = franchise.FranchiseID }, franchise);
+        }
+
+        // DELETE: api/Franchises/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFranchise(int id)
+        {
+            var franchise = await _context.Franchise.FindAsync(id);
+            if (franchise == null)
+            {
+                return NotFound();
+            }
+
+            _context.Franchise.Remove(franchise);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FranchiseExists(int id)
+        {
+            return _context.Franchise.Any(e => e.FranchiseID == id);
+        }
+    }
+}
---
+++ MagicTord-N-SondreTheWebAPI/Services/Characters/CharacterServiceImpl.cs
@@ -0,0 +1,61 @@
+﻿using MagicTord_N_SondreTheWebAPI.Models;
+
+namespace MagicTord_N_SondreTheWebAPI.Services.Characters
+{
+    public class CharacterServiceImpl : ICharacterService
+    {
+
+        private readonly DBContext _dBContext;
+        private readonly ILogger<CharacterServiceImpl> _logger;
+
+        public CharacterServiceImpl(DBContext dBContext, ILogger<CharacterServiceImpl> logger)
+        {
+            _dBContext = dBContext;
+            _logger = logger;
+        }
+
+        public async Task AddAsync(Character entity)
+        {
+            await _dBContext.AddAsync(entity);
+            await _dBContext.SaveChangesAsync();
+            
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var character = await _dBContext.Character.FindAsync(id);
+            if(character == null)
+            {
+                _logger.LogError("Rick Astley fan not found with Id: " + id);
+            }
+        }
+
+        public Task<ICollection<Character>> GetAllAsync()
+        {
+            return await _dBContext.Character
+                .Include(p => p.Movies)
+                .ToListAsync();
+
+        }
+
+        public Task<Character> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Movie>> GetCharacterMoviesAsync(int characterID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Character entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCharacterMoviesAsync(int[] movieIDS, int characterID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
